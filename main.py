c519472905769dc1aecaa04bcd2c71108964b4b7
Первый бот тг <batushka7889@mail.ru>
2024-01-31 18:48:51 +0300

    12332

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('6901567208:AAEm0BlKPWp_ty9XzN1BKpYasPsgcvhXx_0')
 
 @bot.message_handler(commands=['start'])
 def main(msg):
-    bot.send_message(msg.chat.id, 'привет,испытай удачу')
+    bot.send_message(msg.chat.id, 'привет,испытай удачу и посмотри налево')
     markup = telebot.types.InlineKeyboardMarkup(row_width=2)
     but1 = telebot.types.InlineKeyboardButton('Первая', callback_data='first')
     but2 = telebot.types.InlineKeyboardButton('Вторая', callback_data='Second')
